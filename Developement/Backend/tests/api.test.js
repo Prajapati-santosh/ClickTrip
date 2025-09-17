8d12539c3441b281471e0c5e8f8f4aba83552ee6
Santosh Prajapati <101245775+Prajapati-santosh@users.noreply.github.com>
2025-09-17 02:52:52 +0000

    gemini-test

--- Developement/Backend/tests/api.test.js
+++ Developement/Backend/tests/api.test.js
@@ -18,6 +18,19 @@ describe("Tests gemini response", () => {
     }
     it("Expects gemini response to be (500) and response instance", async () => {
         const res = await request(app).post("/gemini").send(obj);
+        expect(res.statusCode).toEqual(200);
+    },300000)
+})
+describe("Tests gemini response", () => {
+    const obj = {
+        "location": "Kyoto, Japan",
+        "budget": "Mid-range (around $1000-$1500 USD per person)",
+        "days_of_stay": 2,
+        "num_of_people": 2,
+        "themes": "Culture, Temples, Food, Nature, Relaxation"
+    }
+    it("Expects gemini response to be (500) and response instance", async () => {
+        const res = await request(app).post("/gemini").send(obj);
         expect(res.statusCode).toEqual(500);
     },300000)
 })
